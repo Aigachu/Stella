1c7f118570a2d4864b0f5c82fc0b32ae5294ea54
Randolph <pelrandolph.rep@gmail.com>
2019-01-25 19:27:28 -0500

    sick picture in the comments

--- stella.js
+++ stella.js
@@ -47,4 +47,4 @@ client.login('token').catch(error => {
 });
 
 // Dango's so cool!
-
+// LOOK AT THAT! https://www.google.com/search?q=retarded+horse&rlz=1C1MSIM_enCA754CA754&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiRqavml4rgAhVq3IMKHRfiD54Q_AUIDigB&biw=1680&bih=939#imgrc=YPMVsm7hfDxmnM:
