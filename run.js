425f8b627b6953960428bf859c7ab23186d3f7ed
Earl <earlj_pal@hotmail.ca>
2019-01-31 00:57:39 -0500

    Changed Zelda's activity to playing smash

--- run.js
+++ run.js
@@ -37,7 +37,7 @@ const client = new DiscordJS.Client();
 client.on('ready', () => {
 
 	// Set game text
-	client.user.setActivity('Bein cute!').catch(console.error); 
+	client.user.setActivity('Super Smash Bros. Ultimate!').catch(console.error); 
 
   // Send a message to the console.
   // You should see this message in the console when you run the bot. You can change this if you want!
