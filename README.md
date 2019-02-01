b863e573549042062a2bffd913c17fbc401205f3
Aigachu <aigachu.sama@gmail.com>
2019-02-01 16:45:53 -0500

    Cleanup, added documentation and prepared for Continuous Integration.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Stella
+
+Hey there! Welcome to Stella, a barebones, minimalistic implementation of a bot built in [discord.js](https://github.com/discordjs/discord.js).
+
+## Requirements
+* Command Line Utility & Knowledge
+    * Basically, you need to know how to navigate around a command line with commands like `cd` and `ls`.
+    * If you're on a Windows computer, it is highly recommended to use a utility like [ConEmu](https://conemu.github.io/) or [Cmder](http://cmder.net/)
+* [Node.js - Latest LTS Version](https://nodejs.org/en/download/)
+* [Git](https://git-scm.com/downloads)
+* A sense of humour
+* A good soul
+
+## Getting Started
+We'll get right into the nitty gritty.
--- run.js
+++ run.js
@@ -5,7 +5,7 @@
  *
  * I'll try to keep this concise and not go too crazy with comments. The README.md file will go in depth.
  *
- * PLEASE NOTE THAT this tutorial assumes that you know minimum coding concepts, as well as what Javascript is. You don't
+ * PLEASE NOTE that this tutorial assumes that you know minimum coding concepts, as well as what Javascript is. You don't
  * need to be a Javascript expert, as the code here will be basic. General coding knowledge should be enough, and
  * this can even be considered practice to become a programmer!
  *
@@ -13,26 +13,42 @@
  */
 
 // Load .env variables.
+// Refer to 'Understanding .env & Environment Variables'. in the README for more info.
 require('dotenv').load();
 
+// We do a quick check to see if a token is set before we do anything else.
+// This avoids us doing all of the code below and just failing at the end when no token is set!
+if (process.env.BOT_TOKEN === undefined) {
+  console.log(`Seems like the token is missing. Can't do anything without a .env file containing a BOT_TOKEN variable! Set that up. :) See .example.env for more details.`);
+  process.exit(1);
+}
+
 // Load the 'discord.js' library. If this library is missing, run 'npm install' in your command line.
+// Refer to 'Understanding Packages' in the README to understand how packages work. :)
 const DiscordJS = require('discord.js');
 
 // Instantiate the Discord Client class.
+// If you don't understand this line, don't worry. Simply know that this line prepares the Discord Client functionality.
 const client = new DiscordJS.Client();
 
 // Define what to do when a 'ready' event occurs.
+// Refer to the 'Understanding Events' part of the README for more information on this. :)
 // Code within this block is executed when a successful connection with Discord is established.
 client.on('ready', () => {
 
   // Send a message to the console.
+  // You should see this message in the console when you run the bot. You can change this if you want!
   console.log(`Logged in as ${client.user.tag}!`);
 
 });
 
 // Define what to do when a 'message' event occurs.
+// Refer to the 'Understanding Events' part of the README for more information on this. :)
 // Code within this block is executed EVERY TIME a message is sent in a channel the bot is in.
+// Think of this line as 'When a message is sent, then...'
 client.on('message', msg => {
+
+  // The magic happens here. You can decide what your bot does every time it "hears" a message.
 
   // If the content of the message is EXACTLY 'ping', make the bot reply to the message with 'Pong!'.
   if (msg.content === 'ping') {
@@ -42,11 +58,8 @@ client.on('message', msg => {
 });
 
 // Make the bot login to Discord.
-// You need to enter your bot's token here. Replace 'token'. Keep the single quotes though.
 // To get your bot's token, visit 'https://discordapp.com/developers/applications/' and create an application.
-// Remember to NEVER commit your token. I'll explain a strategy to save the token elsewhere.
 // You need to create a .env file that contains your bot's token!
-// That file will be ignored by git, so don't worry about it being committed. :)
 client.login(process.env.BOT_TOKEN).catch(error => {
   throw error;
 });
